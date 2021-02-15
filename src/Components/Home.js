a25487732ed85db356aa3cc4e265a614f9d85f9e
amit1884 <amitraj200raj@gmail.com>
2021-02-15 12:58:18 +0530

    some chnages

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,13 @@ function Home() {
     return (
         <div>
             <Navbar/>
+            <div style={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',color:'#fff'}}>
+                <h1 style={{overflow:'hidden'}}>
+                   Lakshmi Chit Fund
+                </h1>
+                <p>Address: Saitan Gully, Khatri Mohalla, Royal Circus ke Picche</p>
+                <p>Contact: 8881212</p>
+            </div>
         </div>
     )
 }
